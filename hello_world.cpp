af0b6b013d771e0691239d8ace038d3d8b490b85
Caylee Annett <caylee.annett@mths.ca>
2021-04-23 14:52:07 +0000

    proper style of hello_world.cpp

---
+++ hello_world.cpp
@@ -0,0 +1,11 @@
+// Copyright (c) 2021 Caylee Annett All rights reserved
+//
+// Created by Caylee Annett
+// Created on April 2021
+// This is the "Hello, World!" program with proper style
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+}
